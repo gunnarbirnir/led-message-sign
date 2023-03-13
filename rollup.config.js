68860e7be25ec3551c58b2c495ad0f51c773a581
Gunnar Birnir Ólafsson <gunnarbirnir@gmail.com>
2023-03-13 13:44:53 +0000

    Compress build output

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,15 @@
+/* eslint-disable import/no-anonymous-default-export */
 import typescript from "rollup-plugin-typescript2";
+import bundleSize from "rollup-plugin-bundle-size";
+import terser from "@rollup/plugin-terser";
 
 import pkg from "./package.json";
 
 export default [
   {
     input: "src/index.ts",
-    output: [
-      { file: pkg.main, format: "cjs" },
-      { file: pkg.module, format: "esm" },
-    ],
-    plugins: [typescript()],
+    output: [{ file: pkg.module, format: "esm" }],
+    plugins: [typescript(), terser(), bundleSize()],
     external: Object.keys(pkg.peerDependencies || {}),
   },
 ];
